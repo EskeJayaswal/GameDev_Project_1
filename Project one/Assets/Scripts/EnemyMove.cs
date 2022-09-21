fbc9df5f093397299ed8cf9480b8d08192c33de8
JonasRex <78900583+JonasRex@users.noreply.github.com>
2022-09-21 20:26:30 +0200

    Added Enemy patrol and attack function

--- Project one/Assets/Scripts/EnemyMove.cs
+++ Project one/Assets/Scripts/EnemyMove.cs
@@ -5,18 +5,116 @@ using UnityEngine.AI;
 
 public class EnemyMove : MonoBehaviour
 {
+   
     public NavMeshAgent agent;
-    GameObject player;
-    // Start is called before the first frame update
-    void Start()
+
+    public GameObject player;
+    public LayerMask whatIsGround, whatIsPlayer;
+    public int health;
+
+    // Patrol
+    public Vector3 walkPoint;
+    bool walkPointSet;
+    public float walkPointRange;
+
+    // Attacking
+    public float timeBetweenAttacks;
+    bool alreadyAttacked;
+
+    // States
+    public float sightRange, attackRange;
+    public bool playerInSightRange, playerInAttackRange;
+
+
+    void Awake()
     {
         player = GameObject.Find("PlayerObject");
         agent = GetComponent<NavMeshAgent>();
+
     }
 
-    // Update is called once per frame
+
     void Update()
     {
+        // Check for sight and attack range
+        playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
+        playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, whatIsPlayer);
+        
+        if (!playerInSightRange && !playerInAttackRange) Patroling();
+        if (playerInSightRange && !playerInAttackRange) ChasePlayer();
+        if (playerInSightRange && playerInAttackRange) AttackPlayer();
+        
+
+        
+    }
+
+    private void Patroling()
+    {
+        if (!walkPointSet) SearchWalkPoint();
+
+        if (walkPointSet)
+            agent.SetDestination(walkPoint);
+
+        Vector3 distanceToWalkPoint = transform.position - walkPoint;
+
+        // Check if walkpoint is reached
+        if (distanceToWalkPoint.magnitude < 1f)
+            walkPointSet = false;
+
+    }
+
+    private void SearchWalkPoint()
+    {
+        // Finde random coordinates within range
+        float randomZ = Random.Range(-walkPointRange, walkPointRange);
+        float randomX = Random.Range(-walkPointRange, walkPointRange);
+
+        walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
+
+        // Check if checkpoint is inside map
+        if (Physics.Raycast(walkPoint, - transform.up, 2f, whatIsGround)) 
+            walkPointSet = true;
+        
+    }
+
+    private void ChasePlayer()
+    {
         agent.SetDestination(player.transform.position);
+    }
+
+    private void AttackPlayer() 
+    {
+        // Make sure enemy doesnt move
+        agent.SetDestination(transform.position);
+
+        transform.LookAt(player.transform);
+
+        if (!alreadyAttacked)
+        {
+            // Attack code
+            Debug.Log("Do Damage!");
+
+            alreadyAttacked = true;
+            Invoke(nameof(ResetAttack), timeBetweenAttacks);
+        }
+
+    }
+
+    private void ResetAttack()
+    {
+        alreadyAttacked = false;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+
+        if (health <= 0) Invoke(nameof(DestroyEnemy), 0.5f);
+
+    }
+
+    private void DestroyEnemy()
+    {
+        Destroy(gameObject);
     }
 }
