84693c06b7d45c89b83de34b99107f24e455d6b0
JonasRex <78900583+JonasRex@users.noreply.github.com>
2022-09-21 14:22:47 +0200

    Minor changes

--- Project one/Assets/Scripts/PlayerMove.cs
+++ Project one/Assets/Scripts/PlayerMove.cs
@@ -5,9 +5,13 @@ using UnityEngine;
 public class PlayerMove : MonoBehaviour
 {
     public CharacterController controller;
+
     public float speed = 12f;
 
-    // Update is called once per frame
+    public float gravity = -20f;
+
+    Vector3 velocity;
+
     void Update()
     {
         float x = Input.GetAxis("Horizontal");
@@ -15,5 +19,9 @@ public class PlayerMove : MonoBehaviour
 
         Vector3 move = transform.right * x + transform.forward * z;
         controller.Move(move * speed * Time.deltaTime);
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
     }
 }
