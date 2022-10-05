f71de41fb02d78ea963a58ab76d78f85d1bcd854
Eske Jayaswal <cph-ej162@cphbusiness.dk>
2022-10-05 11:57:52 +0200

    Reload effect

--- Project one/Assets/Scripts/Gun.cs
+++ Project one/Assets/Scripts/Gun.cs
@@ -1,26 +1,67 @@
 using UnityEngine;
+using System.Collections;
 
 public class Gun : MonoBehaviour
 {
+
     public float damage = 10f;
     public float range = 100f;
+
+    //reload variables
+    public int maxAmmo = 7;
+    private int currentAmmo;
+    public float reloadTime = 3f;
+    private bool isReloading = false;
 
     public Camera fpsCamera;
     public ParticleSystem muzzleFlash;
 
     public GameObject impactEffect;
 
+    public Animator animator;
+
+    void start() 
+    {
+        currentAmmo = maxAmmo;
+    }
+
     void Update()
     {
+        if (isReloading)
+            return;
+
+        if (currentAmmo <= 0)
+        {
+            StartCoroutine(Reload());
+            return;
+        }
+
         if(Input.GetButtonDown("Fire1"))
         {
             Shoot();
         }        
     }
 
+    IEnumerator Reload() 
+    {
+        isReloading = true;
+        Debug.Log("Reloading...");
+
+        animator.SetBool("Reloading", true);
+
+        yield return new WaitForSeconds(reloadTime);
+
+        animator.SetBool("Reloading", false);
+
+        currentAmmo = maxAmmo;
+        isReloading = false;
+    }
+
     void Shoot()
     {
         muzzleFlash.Play();
+
+        currentAmmo--;
 
         RaycastHit hit;
         if (Physics.Raycast(fpsCamera.transform.position, fpsCamera.transform.forward, out hit, range))
@@ -36,4 +77,6 @@ public class Gun : MonoBehaviour
             Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
         }
     }
+
+
 }
