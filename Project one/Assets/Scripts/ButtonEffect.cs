165c85e26dd7e1ed3e15b9d22f7e6f23c9c9cc0c
JonasRex <78900583+JonasRex@users.noreply.github.com>
2022-10-25 18:10:03 +0200

    Button Color changed when pointed at

--- Project one/Assets/Scripts/ButtonEffect.cs
+++ Project one/Assets/Scripts/ButtonEffect.cs
@@ -1,20 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 
 public class ButtonEffect : MonoBehaviour
 {
+    TextMeshProUGUI buttonText;
+    public Color buttonColor;
+    public Color highlightedColor;
+
+    private void Awake()
+    {
+        buttonText = GetComponentInChildren<TextMeshProUGUI>();
+        buttonText.color = buttonColor;
+
+    }
 
 
 
     public void OnPointerEnter()
     {
         transform.localScale = new Vector2(1.5f, 1.5f);
+        buttonText.color = highlightedColor;
+
+        
     }
 
     public void OnPointerExit()
     {
         transform.localScale = new Vector2(1f, 1f);
+        buttonText.color = buttonColor;
     }
 }
