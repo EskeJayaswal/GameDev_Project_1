a8ed5cd678a2fabbc9b7bd48486e868a749551fb
JonasRex <78900583+JonasRex@users.noreply.github.com>
2022-10-25 15:40:28 +0200

    New Night skybox

--- Project one/Assets/Scripts/Menu.cs
+++ Project one/Assets/Scripts/Menu.cs
@@ -13,7 +13,7 @@ public class Menu : MonoBehaviour
 
     public void OnNewGameButton()
     {
-        SceneManager.LoadScene("Game");
+        SceneManager.LoadScene("Game Night");
     }
 
     public void OnQuitButton()
