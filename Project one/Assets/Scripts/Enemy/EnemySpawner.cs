25149ef3f3d24faf989295b1f40329463e17a332
JonasRex <78900583+JonasRex@users.noreply.github.com>
2023-01-03 08:58:07 +0100

    Spawner updated

--- Project one/Assets/Scripts/Enemy/EnemySpawner.cs
+++ Project one/Assets/Scripts/Enemy/EnemySpawner.cs
@@ -16,11 +16,19 @@ public class EnemySpawner : MonoBehaviour
 
     void Update()
     {
-        timeSinceSpawn += Time.deltaTime;
+                timeSinceSpawn += Time.deltaTime;
         if(timeSinceSpawn >= timeToSpawn)
         {
+            Vector3 currentPos = this.transform.position;
+
+            float randomX = Random.Range(-5.0f, 5.0f);
+            float randomZ = Random.Range(-5.0f, 5.0f);
+
+            Vector3 newPos = new Vector3(currentPos.x + randomX ,0, currentPos.z + randomZ);
+
             GameObject newZombie = objectPool.GetZombie();
             newZombie.transform.position = this.transform.position;
+            newZombie.transform.position = newPos;
             timeSinceSpawn = 0f;
         }
     }
