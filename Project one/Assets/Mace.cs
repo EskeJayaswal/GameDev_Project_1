c47079302c3c54eff22d6be38dd2edc9fb7a1224
Eske Jayaswal <cph-ej162@cphbusiness.dk>
2023-01-06 09:27:56 +0100

    Mace script

---
+++ Project one/Assets/Mace.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+
+public class Macce : MonoBehaviour
+{
+    
+    public bool isLocked;
+
+    public float damage = 25f;
+    public float range = 10f;
+
+    public Camera fpsCamera;
+
+    public Animator animator;
+
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            if (!PauseControl.gameIsPaused) 
+                Strike();
+        }
+    }
+
+    void Strike()
+    {
+        animator.SetTrigger("Strike");
+
+        int layerMask = 1 << 11;
+
+        RaycastHit hit;
+        if (Physics.Raycast(fpsCamera.transform.position, fpsCamera.transform.forward, out hit, range, layerMask))
+        {
+            if (hit.collider.GetComponentInParent<IDamagable>() != null)
+            {
+                hit.collider.GetComponentInParent<IDamagable>().TakePhysicalDamage(damage);
+            }
+
+            Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
+        }
+    }
+
+    public void UnlockWeapon()
+    {
+        isLocked = false;
+    }
+}
